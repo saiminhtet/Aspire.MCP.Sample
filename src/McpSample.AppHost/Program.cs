3428e3346fa1bea6893705fcaab4265ce00d90ce
saiminhtet <minhtet_sai@nec.com.sg>
2025-10-29 21:58:04 +0800

    added postgresql mcp server to aspire

--- src/McpSample.AppHost/Program.cs
+++ src/McpSample.AppHost/Program.cs
@@ -3,9 +3,15 @@ var builder = DistributedApplication.CreateBuilder(args);
 var aspnetsseserver = builder
     .AddProject<Projects.McpSample_AspNetCoreSseServer>("aspnetsseserver")
     .WithExternalHttpEndpoints();
+
+var postgresqlmcpserver = builder
+    .AddProject<Projects.McpSample_PostgreSQLMCPServer>("postgresqlmcpserver")
+    .WithExternalHttpEndpoints();
+
 var blazorchat = builder
     .AddProject<Projects.McpSample_BlazorChat>("blazorchat")
     .WithReference(aspnetsseserver)
+    .WithReference(postgresqlmcpserver)
     .WithExternalHttpEndpoints();
 
 builder.Build().Run();
